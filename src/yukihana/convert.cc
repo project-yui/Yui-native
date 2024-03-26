4b08b5766a4f83dd8de416bc8af635d0100c0761
msojocs <jiyecafe@gmail.com>
2024-03-26 09:55:13 +0800

    feat: make protoc for windows

--- src/yukihana/convert.cc
+++ src/yukihana/convert.cc
@@ -3,6 +3,29 @@
 #include "spdlog/spdlog.h"
 
 namespace nt_convert {
+    
+    /**
+     * 消息元素类型
+     * 
+     * 1 - 纯文本&@ textElement
+     * 
+     * 2 - 图片 picElement
+     * 
+     * 5 - 视频 videoElement
+     * 
+     * 6 - 表情 faceElement
+     * 
+     * 7 - 引用回复 replyElement
+     * 
+     * 8 - 提示消息 [撤回]
+     * 
+     * 10 - ArkElement
+     * 
+     * 11 - 商城表情 marketFaceElement
+     * 
+     * 16 - 转发消息 multiForwardMsgElement
+     * 
+     */
     static std::map<int32_t, convert> convertTable;
 
     /**
@@ -15,6 +38,7 @@ namespace nt_convert {
     {
         dest->set_elementtype(nt_msg::Element_MsgType_MSG_TYPE_TEXT);
         
+        // elementId
         auto elementId = src.Get("elementId").As<Napi::String>();
         dest->set_elementid(atol(elementId.Utf8Value().c_str()));
 
@@ -24,8 +48,107 @@ namespace nt_convert {
         dest->set_attype(0);
     }
 
+    /**
+     * @brief 图片元素转换
+     * 
+     * @param dest protobuf对象
+     * @param src NAPI对象
+     */
+    void picElement2Protobuf(nt_msg::Element *dest, Napi::Object &src)
+    {
+        // export interface PicElementType {
+        //     /**
+        //     * 0 - 普通图片
+        //     * 
+        //     * 1 - 表情图片
+        //     */
+        //     picSubType: 0 | 1
+        //     fileName: `{${string}}.${string}`
+        //     fileSize: `${number}`
+        //     picWidth: number
+        //     picHeight: number
+        //     original: boolean
+        //     md5HexStr: string
+        //     sourcePath: string
+        //     thumbPath: {}
+        //     transferStatus: number
+        //     progress: number
+        //     picType: number
+        //     invalidState: number
+        //     fileUuid: `${number}`
+        //     fileSubId: string
+        //     thumbFileSize: number
+        //     fileBizId:null
+        //     downloadIndex:null
+        //     summary: string
+        //     emojiFrom: null
+        //     emojiWebUrl: null
+        //     emojiAd:{
+        //     url: string
+        //     desc: string
+        //     }
+        //     emojiMall:{
+        //     packageId: number
+        //     emojiId: number
+        //     },
+        //     emojiZplan:{
+        //     actionId: 0,
+        //     actionName: string,
+        //     actionType: 0,
+        //     playerNumber: 0,
+        //     peerUid: `${number}`
+        //     bytesReserveInfo: string
+        //     },
+        //     originImageMd5: string
+
+        //     /**
+        //     * 图片网络地址
+        //     * 
+        //     * 没有host, https://gchat.qpic.cn/
+        //     * 
+        //     * /gchatpic_new/发送者QQ/群号-uuid-MD5/0
+        //     */
+        //     originImageUrl: string
+        //     import_rich_media_context: null,
+        //     isFlashPic: boolean
+        // }
+        dest->set_elementtype(nt_msg::Element_MsgType_MSG_TYPE_TEXT);
+        
+        // elementId
+        auto elementId = src.Get("elementId").As<Napi::String>();
+        dest->set_elementid(atol(elementId.Utf8Value().c_str()));
+
+        auto picElement = src.Get("picElement").As<Napi::Object>();
+
+        {
+            auto fileName = picElement.Get("fileName").As<Napi::String>();
+            dest->set_filename(fileName.Utf8Value());
+
+            auto fileSize = picElement.Get("fileSize").As<Napi::Number>();
+            dest->set_filesize(fileSize.Int32Value());
+
+            auto picWidth = picElement.Get("picWidth").As<Napi::Number>();
+            dest->set_picwidth(picWidth.Int32Value());
+            
+            auto picHeight = picElement.Get("picHeight").As<Napi::Number>();
+            dest->set_picheight(picHeight.Int32Value());
+            
+            auto picType = picElement.Get("picType").As<Napi::Number>();
+            dest->set_pictype(picType.Int32Value());
+            
+            auto fileUuid = picElement.Get("fileUuid").As<Napi::String>();
+            dest->set_fileuuid(fileUuid.Utf8Value());
+            
+            auto originImageUrl = picElement.Get("originImageUrl").As<Napi::String>();
+            dest->set_originimageurl(originImageUrl.Utf8Value());
+        }
+        
+    }
     ElementConverter::ElementConverter() {
+        // 文本
         this->convertTable.emplace(std::pair<int32_t, convert>(1, textElement2Protobuf));
+        // 图片
+        this->convertTable.emplace(std::pair<int32_t, convert>(2, picElement2Protobuf));
     }
     ElementConverter::~ElementConverter() {
 

6fb6990c7f470ad7de2ef0a37aa81d162b01eb64
msojocs <jiyecafe@gmail.com>
2024-04-17 09:23:09 +0800

    feat: release with tag

--- tools/windows/ci.ps1
+++ tools/windows/ci.ps1
@@ -1,3 +1,4 @@
+param($arch, $tag)
 $root_dir = Resolve-Path (Join-Path $PSScriptRoot "../../")
 
 cd $root_dir
@@ -6,7 +7,7 @@ try
     cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -S"$root_dir" -B"$root_dir/build" -G Ninja
     cmake --build "$root_dir/build" --config Debug --target nt_native --
     mkdir "$root_dir/tmp/build"
-    mv "$root_dir/build/nt_native.node" "$root_dir/tmp/build/telecord-native-win32.node"
+    mv "$root_dir/build/nt_native.node" "$root_dir/tmp/build/telecord-native-win32-$arch-$tag.node"
 }catch{
     exit 1
 }
